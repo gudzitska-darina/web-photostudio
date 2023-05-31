480bffbc312031ba72450b233e45e68d23ca3f2e
gudzitska-darina <71644904+gudzitska-darina@users.noreply.github.com>
2023-05-31 19:10:28 +0300

    finish

---
+++ tgbot.js
@@ -0,0 +1,23 @@
+const TOKEN = "6072392573:AAGNV6_pw4PZAzxIWVef5C9hUx1nsf1SEMo"; // токен от BotFather
+const CHAT_ID = "408233164"; // chat_id для телеграм
+
+var form = document.querySelector(".form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  data = new FormData(this);
+  sendMsg(data);
+});
+
+function sendMsg(data) {
+  var url = "https://api.telegram.org/bot" + TOKEN + "/sendMessage";
+  var body = JSON.stringify({
+    chat_id: CHAT_ID,
+    parse_mode: "Markdown",
+    text: "Вітаємо, " + data.get("name") + "!\nНайближчим часом з вами звʼяжеться оператор",
+  });
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+  xhr.send(body);
+  form.reset();
+}
